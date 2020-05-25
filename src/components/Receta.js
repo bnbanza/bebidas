91c7dfbef2dec05e420c68d2adee2a3e59a43f95
bnbanza <bnbanza@gmail.com>
2020-05-25 07:28:54 -0300

    first commit

---
+++ src/components/Receta.js
@@ -0,0 +1,112 @@
+import React, {useContext, useState} from 'react';
+import {ModalContext} from '../context/ModalContext';
+import PropTypes from 'prop-types';
+
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+
+function getModalStyle() {
+    const top = 50 ;
+    const left = 50;
+  
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+}
+
+const useStyles = makeStyles(theme => ({
+    paper: {
+      position: 'absolute',
+      width: 450,
+      height: 800,
+      backgroundColor: theme.palette.background.paper,
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+      overflow: 'auto',
+    },
+}));
+
+const Receta = ({receta}) => {
+
+    //configuracion del modal de Material-UI
+    const [modalStyle] = useState(getModalStyle);
+    const [open, setOpen] = useState(false);
+    
+    
+    const classes = useStyles();
+
+    const handleOpen = () => {
+        setOpen(true);
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    }
+
+    //extraer los valores del context
+    const {inforeceta, guardarIdReceta, guardarReceta} = useContext(ModalContext);
+
+   //Muestra y formatea los ingredientes
+
+   const mostrarIngredientes = inforeceta => {
+       let ingredientes = [];
+       for(let i = 1; i < 16; i++){
+           if(inforeceta[`strIngredient${i}`]){
+               ingredientes.push(
+               <li>{inforeceta[`strIngredient${i}`]} {inforeceta[`strMesure${i}`]} </li>
+               )
+           }
+       }
+       return ingredientes;
+   }
+
+    return ( 
+        <div className="col-md-4 mb-3">
+            <div className="card">
+                <h2 className="card-header" style={{height: 95}}>{receta.strDrink}</h2>
+
+                <img className="card-img-top" src={receta.strDrinkThumb} alt={`Imagen de ${receta.strDrink}`}/>
+                <div className="card-body">
+                    <button
+                        type="button"
+                        className="btn btn-block btn-primary"
+                        onClick={() => {
+                            guardarIdReceta(receta.idDrink)
+                            handleOpen();
+                        }}
+                    >Ver Receta</button>
+                    <Modal
+                        open={open}
+                        onClose={() => {
+                            guardarIdReceta(null);
+                            guardarReceta({});
+                            handleClose();
+                            
+                        }}
+                    >
+                        <div style={modalStyle} className={classes.paper}>
+                            <h2>{inforeceta.strDrink}</h2>
+                            <h3 className="mt-4">Instrucciones</h3>
+                            <p>
+                                {inforeceta.strInstructions}
+                            </p>
+                            <img className="img-fluid my-4" src={inforeceta.strDrinkThumb} alt={inforeceta.strDrink}/>
+                            <h3>Ingredientes y cantidades</h3>
+                            <ul>
+                                {mostrarIngredientes(inforeceta)}
+                            </ul>
+                        </div>
+                    </Modal>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+Receta.propTypes = {
+    receta: PropTypes.object.isRequired
+}
+
+export default Receta;
